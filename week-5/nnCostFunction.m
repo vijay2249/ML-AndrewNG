deeadb4367e3c18818b5a5d2465d61e90ef14e19
V1ZEI-CN <vijaychowdary3699@gmail.com>
2022-01-04 19:58:00 +0530

    reg term calc error fixed

--- week-5/nnCostFunction.m
+++ week-5/nnCostFunction.m
@@ -85,16 +85,17 @@ s = a3;  % h(x)
 vy = (1:num_labels) == y;
 
 % cost function without regularization
-J = (1/m)*sum((-vy.*log(s))-((1-vy).*(log(1-s))));
+J = (1/m)*sum(sum((-vy.*log(s))-((1-vy).*log(1-s))));
+%J = (1/m)*sum((-vy.*log(s))-((1-vy).*(log(1-s))));
 
 % =========== back propagation implementation and calculating regularization term in cost function
 
 % move forward
 A1 = X;
+Z2 = A1*Theta1';
 A2 = sigmoid(A1*Theta1');
 A2 = [ones(size(A2,1),1), A2];
-Z2 = A2*Theta2';
-A3 = sigmoid(Z2);
+A3 = sigmoid(A2*Theta2');
 s = A3;  % h(x)
 VY = (1:num_labels) == y;
 % get the differences
@@ -106,9 +107,15 @@ Theta1_grad = (1/m)*(D2'*A1);
 Theta2_grad = (1/m)*(D3'*A2);
 
 
+% ==== Regularisation term calculation
+reg = (lambda/(2*m))*(sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end))));
+J = J + reg;
 
+theta1_reg = (lambda/m)*([zeros(size(Theta1, 1), 1) Theta1(:, 2:end)]);
+theta2_reg = (lambda/m)*([zeros(size(Theta2, 1), 1) Theta2(:, 2:end)]);
 
-
+Theta1_grad = Theta1_grad + theta1_reg;
+Theta2_grad = Theta2_grad + theta2_reg;
 
 % -------------------------------------------------------------
 
