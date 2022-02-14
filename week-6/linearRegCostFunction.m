1b621669aee88ea7000d35bb7d080ea597a592e5
V1ZEI-CN <vijaychowdary3699@gmail.com>
2022-02-14 14:28:56 +0530

    regularized linear regression + bias/variance + poly-regression

--- week-6/linearRegCostFunction.m
+++ week-6/linearRegCostFunction.m
@@ -19,16 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-s = X*theta;
-
-J = (1/(2*m))*sum((s-y).^2);
-
-reg = (lambda/(2*m))*sum(theta(2:end).^2);
-
-J = J + reg;
-
-grad(1) = (1/m)*(X(:,1)'*(s-y));
-grad(2:end) = (1/m)*(X(:,2:end)'*(s-y))+(lambda/m)*theta(2:end);
+  s = X*theta;
+  J = (1/(2*m))*sum((s-y).^2);
+  reg = (lambda/(2*m))*sum(theta(2:end).^2);
+  J = J + reg;
+  grad(1) = (1/m)*(X(:,1)'*(s-y));
+  grad(2:end) = (1/m)*(X(:,2:end)'*(s-y))+(lambda/m)*theta(2:end);
 
 
 % =========================================================================
